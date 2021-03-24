4ad7ecfdbfb477f91a1b16b64fc8d3963692bcc1
Hellokugou520 <57349905+Hellokugou520@users.noreply.github.com>
2021-03-24 16:17:43 +0800

    change

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const images = document.querySelectorAll('header>div>img')
+const images = document.querySelectorAll("header>div>img");
 
-document.querySelector('header').addEventListener('mousemove', (e) => {
-    let percentage = e.clientX / window.outerWidth
+document.querySelector("header").addEventListener("mousemove", (e) => {
+  let percentage = e.clientX / window.outerWidth;
 
-    let offset = 10 * percentage
-    let blur = 20
+  let offset = 10 * percentage;
+  let blur = 20;
 
-    for (let [index, image] of images.entries()) {
-        offset *= 1.3
+  for (let [index, image] of images.entries()) {
+    offset *= 1.3;
 
-        let blurValue = (Math.pow((index / images.length - percentage), 2) * blur)
+    let blurValue = Math.pow(index / images.length - percentage, 2) * blur;
 
-        image.style.setProperty('--offset', `${offset}px`)
-        image.style.setProperty('--blur', `${blurValue}px`)
-    }
-})
+    image.style.setProperty("--offset", `${offset}px`);
+    image.style.setProperty("--blur", `${blurValue}px`);
+  }
+});
